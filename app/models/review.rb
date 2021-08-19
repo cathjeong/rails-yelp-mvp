ea1e663e8d92f9a183bb28c08aac20afe37a49d1
Catherine Jeong <cathjeong@gmail.com>
2021-08-19 18:47:27 +0900

    Yelp MVP draft v.1

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: 0..5
   validates :content, presence: true
 end
